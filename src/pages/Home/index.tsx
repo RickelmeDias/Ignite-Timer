f790055a1291557ce5905e2bb145aeeceaca0a18
Rickelme Dias <rickelmegadias@gmail.com>
2023-01-02 16:07:19 -0300

    Zod form validation lib integrated with react hooks

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import {
   CountdownContainer,
@@ -11,15 +13,25 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser no máximo 60 minutos'),
+})
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
 
   function handleCreateNewCycle(data: any) {
     console.log(data)
     // event.target.task.value;
   }
 
-  const taskIsEmpty = watch('task').trim() === ''
+  const taskIsEmpty = watch('task') === undefined || watch('task').trim() === ''
 
   return (
     <HomeContainer>
@@ -46,7 +58,8 @@ export function Home() {
             type="number"
             placeholder="00"
             step={5}
-            min={0}
+            min={5}
+            max={60}
             {...register('minutesAmount', { valueAsNumber: true })}
           />
 
