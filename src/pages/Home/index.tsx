13ff640e0797becf37f366663e0b7ed2334d5aca
Rickelme Dias <rickelmegadias@gmail.com>
2023-01-02 16:43:00 -0300

    clear fields to the defaultValues using reset

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,7 +23,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -33,7 +33,7 @@ export function Home() {
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
-    // event.target.task.value;
+    reset()
   }
 
   const taskIsEmpty = watch('task') === undefined || watch('task').trim() === ''
